8a822dd98da21791e5804bb77487105ada4b10a0
Ryu <ryu.siegert@gmail.com>
2026-08-03 13:18:40 +0200

    Added further details to SlotsGameDTO

--- slots/src/main/java/casino/slots/view/SlotsGameDTO.java
+++ slots/src/main/java/casino/slots/view/SlotsGameDTO.java
@@ -1,7 +1,11 @@
 package casino.slots.view;
 
+import casino.slots.domain.enums.Symbol;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -9,5 +13,9 @@ public class SlotsGameDTO {
 
   private long user;
   private long id;
+  private boolean winning;
+  private BigDecimal resultingAmount;
+  private BigDecimal betAmount;
+  private List<Symbol> symbols;
 
 }
