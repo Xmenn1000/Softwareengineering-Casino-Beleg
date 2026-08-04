4aa2dc76e1f919436bf8acc7f6c3595ad036f59f
Ryu <ryu.siegert@gmail.com>
2026-08-04 22:53:40 +0200

    Updated SlotsGameEntity

--- slots/src/main/java/casino/slots/model/SlotsGameEntity.java
+++ slots/src/main/java/casino/slots/model/SlotsGameEntity.java
@@ -2,6 +2,8 @@ package casino.slots.model;
 
 import casino.slots.domain.dto.OutCome;
 import casino.slots.domain.enums.Symbol;
+import casino.slots.validation.SlotsGameEntityValidator;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,18 +19,44 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-@Table(name = "slotGame")
+@Table(name = "SlotsGames")
 @Entity
 public class  SlotsGameEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "Id", nullable = false, updatable = false)
   private long id;
 
+  @Column(name = "UserId", nullable = false)
   private long userId;
+
+  @Column(name = "Winning", nullable = false)
   private boolean winning;
+
+  @Column(name = "Amount", nullable = false)
   private BigDecimal amount;
+
+  @Column(name = "BetAmount", nullable = false)
   private BigDecimal betAmount;
+
+  @Column(name = "SlotStates", nullable = false)
   private List<Symbol> slotStates;
+
+  public static SlotsGameEntity create(
+          Long user,
+          boolean winning,
+          BigDecimal amount,
+          BigDecimal betAmount,
+          List<Symbol> slotStates
+  ) {
+    SlotsGameEntityValidator validator = new SlotsGameEntityValidator();
+
+    SlotsGameEntity entity = SlotsGameEntityFactory.create(user, winning, amount, betAmount, slotStates);
+
+    validator.validate(entity);
+
+    return entity;
+  }
 
 }
