09153455ce5a73507533c2aa524756de14e4c50f
Ryu <ryu.siegert@gmail.com>
2026-08-05 15:31:29 +0200

    Introducing GameHistoryServiceImpl

--- slots/src/main/java/casino/slots/service/GameHistoryServiceImpl.java
+++ slots/src/main/java/casino/slots/service/GameHistoryServiceImpl.java
@@ -1,5 +1,9 @@
 package casino.slots.service;
 
+import casino.slots.exeptions.SlotsGameNotFoundException;
+import casino.slots.mapper.SlotsGameMapper;
+import casino.slots.model.SlotsGameEntity;
+import casino.slots.repository.SlotsGameRepository;
 import casino.slots.view.SlotsGameDTO;
 import org.springframework.stereotype.Service;
 
@@ -8,18 +12,39 @@ import java.util.List;
 @Service
 public class GameHistoryServiceImpl implements GameHistoryService {
 
+    private final SlotsGameRepository repository;
+
+    public GameHistoryServiceImpl(SlotsGameRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public List<SlotsGameDTO> findAll() {
-        throw new UnsupportedOperationException("not implemented yet");
+        return repository.findAll()
+                .stream()
+                .map(SlotsGameMapper::toDTO)
+                .toList();
     }
 
     @Override
     public SlotsGameDTO findById(Long gameId) {
-        throw new UnsupportedOperationException("not implemented yet");
+        SlotsGameEntity game = repository.findById(gameId)
+                .orElseThrow(() ->
+                        new SlotsGameNotFoundException(gameId)
+                );
+
+        return SlotsGameMapper.toDTO(game);
     }
 
     @Override
     public SlotsGameDTO deleteById(Long gameId) {
-        throw new UnsupportedOperationException("not implemented yet");
+        SlotsGameEntity game = repository.findById(gameId)
+                .orElseThrow(() ->
+                        new SlotsGameNotFoundException(gameId)
+                );
+
+        repository.delete(game);
+
+        return SlotsGameMapper.toDTO(game);
     }
 }
